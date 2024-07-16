40d36554699096cce8c66a906e17b7057efa7fde
Raj Karmakar <97rajmath@gmail.com>
2024-07-16 13:53:15 +0530

    Started with LoginPage

--- twitter-ui/src/App.tsx
+++ twitter-ui/src/App.tsx
@@ -1,8 +1,19 @@
+import { ThemeProvider } from "@emotion/react"
+import BackgroundModeContextProvider from "./context/BackgroundModeContext/BackgroundModeContextProvider"
+import ColorModeContextProvider from "./context/ColorModeContext/ColorModeContextProvider"
+import Theme from "./utils/Theme"
+import { CssBaseline } from "@mui/material"
+
 const App = () => {
   return (
-    <>
-      App
-    </>
+    <BackgroundModeContextProvider>
+      <ColorModeContextProvider>
+        <ThemeProvider theme={Theme}>
+          <CssBaseline />
+          App
+        </ThemeProvider>
+      </ColorModeContextProvider>
+    </BackgroundModeContextProvider>
   )
 }
 
