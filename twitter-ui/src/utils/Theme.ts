b24660bd3fc98ea0a99ed3784ad20ec8610be995
Raj Karmakar <97rajmath@gmail.com>
2024-07-16 12:55:08 +0530

    Intial project setup & adding twitter color modes list

---
+++ twitter-ui/src/utils/Theme.ts
@@ -0,0 +1,59 @@
+import { PaletteMode } from "@mui/material";
+import { amber, green, grey } from "@mui/material/colors";
+
+export const COLOR_MODES = {
+	BLUE: "Blue",
+	YELLOW: "Yellow",
+	PINK: "Pink",
+	PURPLE: "Purple",
+	ORANGE: "Orange",
+	GREEN: "Green",
+}
+
+export const BACKGROUND_MODES = {
+	DEFAULT: "Default",
+	DIM: "Dim",
+	LIGHTS_OUT: "Lights Out",
+}
+
+declare module "@mui/material/styles" {
+	interface TypeText {
+		success: string,
+		info: string,
+	}
+  }
+
+const Theme = (mode: PaletteMode) => ({
+	palette: {
+		mode,
+		...(mode === 'light'
+		? {
+			// palette values for light mode
+			primary: amber,
+			divider: grey[700],
+			text: {
+				primary: grey[900],
+				secondary: grey[800],
+				success: green['A700'],
+				info: '#ed6c02',
+			},
+		}
+		: {
+			// palette values for dark mode
+			primary: grey,
+			divider: grey[300],
+			background: {
+				default: grey[900],
+				paper: grey[900],
+			},
+			text: {
+				primary: grey[50],
+				secondary: grey[500],
+				success: green['A400'],
+				info: '#29b6f6',
+			},
+		}),
+	},
+  });
+
+  export default Theme
