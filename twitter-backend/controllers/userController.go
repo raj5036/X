fd7da3733c204d3df6a03ab0696e081b5952e742
Raj Karmakar <97rajmath@gmail.com>
2024-07-21 16:20:27 +0530

    load .env file variables using godotenv package

--- twitter-backend/controllers/userController.go
+++ twitter-backend/controllers/userController.go
@@ -1,6 +1,28 @@
 package controllers
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/raj5036/x/utils"
+)
+
+func init() {
+	err := godotenv.Load()
+	utils.HandleError(err)
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+
+	fmt.Printf("DB_HOST: %s\n", dbHost)
+	fmt.Printf("DB_PORT: %s\n", dbPort)
+	fmt.Printf("DB_USER: %s\n", dbUser)
+	fmt.Printf("DB_PASSWORD: %s\n", dbPassword)
+}
 
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
