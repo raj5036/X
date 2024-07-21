3f3fae46d1d972823748f1ecf0bee42e258d01f6
Raj Karmakar <97rajmath@gmail.com>
2024-07-21 10:58:04 +0530

    Use GoogleLogin button provided by @react-oauth

--- twitter-ui/src/pages/LoginPage/LoginPage.tsx
+++ twitter-ui/src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import XLogo from '../../assets/images/logo-white.png'
 import { LoginPageStyles } from "./LoginPageStyles";
-import { Stack, Typography } from "@mui/material";
-import GoogleLoginButton from "../../components/GoogleLoginButton/GoogleLoginButton";
+import { Stack, Typography, useTheme } from "@mui/material";
+import { CredentialResponse, GoogleLogin } from "@react-oauth/google";
 
 const LoginPage: React.FC = () => {
+	const theme = useTheme()
+
+	const handleGoogleLoginSuccess = (credentialResponse: CredentialResponse) => {
+		console.log(credentialResponse);
+	}
+
+	const handleGoogleLoginFailure = () => {
+		console.log('Login Failed');
+	}
+
 	return (
 		<LoginPageStyles.Container>
 			<Stack direction={"row"} justifyContent={"center"} alignItems={"center"}>
@@ -12,7 +22,16 @@ const LoginPage: React.FC = () => {
 				<LoginPageStyles.LoginFormContainer>
 					<Typography variant="h2" fontWeight={"bold"}>Happening now</Typography>
 					<Typography variant="h4">Join today.</Typography>
-					<GoogleLoginButton />
+					<GoogleLogin
+						size="large"
+						shape="pill"
+						logo_alignment="center"
+						theme="outline"
+						width={theme.spacing(35)}
+						cancel_on_tap_outside={true}
+						onSuccess={handleGoogleLoginSuccess}
+						onError={handleGoogleLoginFailure}
+				/>
 				</LoginPageStyles.LoginFormContainer>
 			</Stack>
 		</LoginPageStyles.Container>
